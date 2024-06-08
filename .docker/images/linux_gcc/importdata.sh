b9c73ed303b148911e8d3534a3e822688be7020c
pctronique <pctronique@free.fr>
2024-06-08 20:31:38 +0200

    modif image docker

--- .docker/images/linux_gcc/importdata.sh
+++ .docker/images/linux_gcc/importdata.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-if [ -z ${JAVA_FOLDER_PROJECT} ]
+if [ -z ${GCC_FOLDER_PROJECT} ]
 then
-    JAVA_FOLDER_PROJECT=/home/project/
+    GCC_FOLDER_PROJECT=/home/project/
 fi
 
-if [ -z ${JAVA_FOLDER_LOG} ]
+if [ -z ${GCC_FOLDER_LOG} ]
 then
-    JAVA_FOLDER_LOG=/var/log/docker/java/
+    GCC_FOLDER_LOG=/var/log/docker/java/
 fi
 
-if [ -z ${JAVA_FOLDER_DATA} ]
+if [ -z ${GCC_FOLDER_DATA} ]
 then
-    JAVA_FOLDER_DATA=data
+    GCC_FOLDER_DATA=data
 fi
 
-if [ -z ${JAVA_FOLDER_INIT_DATA} ]
+if [ -z ${GCC_FOLDER_INIT_DATA} ]
 then
-    JAVA_FOLDER_INIT_DATA=/docker-entrypoint-initdata.d/
+    GCC_FOLDER_INIT_DATA=/docker-entrypoint-initdata.d/
 fi
 
-if [ ! -d "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}" ]; then
-    if [ ! -z "$(ls -A ${JAVA_FOLDER_INIT_DATA})" ]; then
-        mkdir -p "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}" 2>> ${JAVA_FOLDER_LOG}/installdata.log
-        cp -r ${JAVA_FOLDER_INIT_DATA}/* "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}/" 2>> ${JAVA_FOLDER_LOG}/installdata.log
-        chmod 777 -R "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}"
+if [ ! -d "${GCC_FOLDER_PROJECT}/${GCC_FOLDER_DATA}" ]; then
+    if [ ! -z "$(ls -A ${GCC_FOLDER_INIT_DATA})" ]; then
+        mkdir -p "${GCC_FOLDER_PROJECT}/${GCC_FOLDER_DATA}" 2>> ${GCC_FOLDER_LOG}/installdata.log
+        cp -r ${GCC_FOLDER_INIT_DATA}/* "${GCC_FOLDER_PROJECT}/${GCC_FOLDER_DATA}/" 2>> ${GCC_FOLDER_LOG}/installdata.log
+        chmod 777 -R "${GCC_FOLDER_PROJECT}/${GCC_FOLDER_DATA}"
     fi
 fi
 
